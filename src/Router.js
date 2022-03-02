a42f50b793de6fe75fb45ff7363cb5e91d5c7d5d
dlwjdals <jml22zzang@gmail.com>
2022-03-02 11:59:05 +0900

    Add : first commit. 초기 세팅 완료.

---
+++ src/Router.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+// 다송's 컴포넌트
+import Logindasongyu from './pages/dasongyu/Login/Login';
+import Maindasongyu from './pages/dasongyu/Main/Main';
+
+// 근홍's 컴포넌트
+import LogingeunhongLim from './pages/geunhongLim/Login/Login';
+import MaingeunhongLim from './pages/geunhongLim/Main/Main';
+
+// 정민's 컴포넌트
+import Loginjeongminlee from './pages/jeongminlee/Login/Login';
+import Mainjeongminlee from './pages/jeongminlee/Main/Main';
+
+// 지호's 컴포넌트
+import Loginjihokwon from './pages/jihokwon/Login/Login';
+import Mainjihokwon from './pages/jihokwon/Main/Main';
+
+// 민경's 컴포넌트
+import Loginminkyoungkim from './pages/minkyoungkim/Login/Login';
+import Mainminkyoungkim from './pages/minkyoungkim/Main/Main';
+
+// 송이's 컴포넌트
+import Loginsongyipark from './pages/songyipark/Login/Login';
+import Mainsongyipark from './pages/songyipark/Main/Main';
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login-dasongyu' element={<Logindasongyu />} />
+        <Route path='/main-dasongyu' element={<Maindasongyu />} />
+
+        <Route path='/login-geunhongLim' element={<LogingeunhongLim />} />
+        <Route path='/main-geunhongLim' element={<MaingeunhongLim />} />
+
+        <Route path='/login-jeongminlee' element={<Loginjeongminlee />} />
+        <Route path='/main-jeongminlee' element={<Mainjeongminlee />} />
+
+        <Route path='/login-jhokwon' element={<Loginjihokwon />} />
+        <Route path='/main-jihokwon' element={<Mainjihokwon />} />
+
+        <Route path='/login-minkyoungkim' element={<Loginminkyoungkim />} />
+        <Route path='/main-minkyoungkim' element={<Mainminkyoungkim />} />
+
+        <Route path='/login-songyipark' element={<Loginsongyipark />} />
+        <Route path='/main-songyipark' element={<Mainsongyipark />} />
+        
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
