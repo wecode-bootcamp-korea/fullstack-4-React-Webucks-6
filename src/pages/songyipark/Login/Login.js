4cb4778017b4a016f1d47ba4b04c1434318cbb47
songyi-p <songyi7091@gmail.com>
2022-03-02 14:55:57 +0900

    [Add] : login component 구현

---
+++ src/pages/songyipark/Login/Login.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Login.scss";
+import { Link } from "react-router-dom";
+
+function Login() {
+  function loginBtn() {}
+
+  return (
+    <div className="login-full-page">
+      <main className = "login-container">
+        <img className = "webucks-img" alt="webucks 로고" src="./images/webucks.png" />
+        <section className="login-section">
+          <input
+            className="id"
+            type="text"
+            placeholder="전화번호, 사용자 이름 또는 이메일"
+          />
+          <input className="pw" type="password" placeholder="비밀번호" />
+          <button
+            className="login-btn"
+            // disabled="true"
+            onClick={() => {
+              loginBtn();
+            }}
+          >
+            <Link to="/List">로그인</Link>
+          </button>
+        </section>
+        <div className="pw-question">비밀번호를 잊으셨나요?</div>
+      </main>
+    </div>
+  );
+}
+
+export default Login;
