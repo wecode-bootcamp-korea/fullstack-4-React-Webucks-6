ede02874a1d6a629a0fa2c8854d82b0b72bf4f67
xxziiko <xxziiko@gmail.com>
2022-03-03 19:26:24 +0900

    Modify: Login.js, Login.scss 수정

--- src/pages/jihokwon/Login/Login.js
+++ src/pages/jihokwon/Login/Login.js
@@ -1,23 +1,28 @@
-import React from "react";
-import { Link, useNavigate } from "react-router-dom";
-import "./Login.scss";
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import './Login.scss';
 
 function LoginComponent() {
   const navigate = useNavigate();
 
   const goToList = () => {
-    navigate("/list-jihokwon");
+    navigate('/list-jihokwon');
   };
   return (
     <div>
       <div className="container">
         <div className="loginArea">
           <div className="title">
-            <img src="/images/jihokwon/images/WeBucks.png" alt="" />
+            <img src="/images/logo.png" alt="" />
           </div>
-          <input type="text" placeholder="전화번호, 사용자 이름 또는 이메일" />
-          <input type="password" placeholder="비밀번호" />
-          <button onClick={goToList}>로그인</button>
+          <div>
+            <input
+              type="text"
+              placeholder="전화번호, 사용자 이름 또는 이메일"
+            />
+            <input type="password" placeholder="비밀번호" />
+            <button onClick={goToList}>로그인</button>
+          </div>
           <Link to="/signup" className="find-password" href="">
             비밀번호를 잊으셨나요?
           </Link>
