c450ee07e4fecc471aa531066323abad3e4390c2
songyi-p <songyi7091@gmail.com>
2022-03-03 19:06:44 +0900

    [Modify] : songyipark2 branch 수정

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Router from './Router'
-import './styles/reset.scss'
-import './styles/common.scss'
-import './styles/variables.scss'
-
+import Router from './Router';
+import './styles/reset.scss';
+import './styles/common.scss';
+import './styles/variables.scss';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
