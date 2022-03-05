251a1192879d2542c56d5252486a0dad7772aed1
geunhong Lim <seelime11@gmail.com>
2022-03-06 00:24:14 +0900

    [add] Misson2 완료

--- src/pages/geunhongLim/pages/Login/Login.js
+++ src/pages/geunhongLim/pages/Login/Login.js
@@ -1,20 +1,40 @@
 import styles from './Login.module.scss';
 import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 function Login() {
   const navigate = useNavigate();
+  const [idValue, setIdValue] = useState('');
+  const [password, setPassword] = useState();
 
   const goToList = () => {
     navigate('/main-geunhongLim');
   };
+
+  const handleIdInput = event => {
+    return event.target.value;
+  };
+
   return (
     <section className={styles.loginBox}>
-      <figure className={styles.logo}>
+      <figure className={styles.ghLogo}>
         <img src="images/logo.png" alt="logo" />
       </figure>
-      <form className={styles.loginFormBox}>
-        <input type="text" placeholder="전화번호, 사용자 이름 또는 이메일" />
-        <input type="password" placeholder="비밀번호" />
+      <div className={styles.loginFormBox}>
+        <input
+          type="text"
+          placeholder="전화번호, 사용자 이름 또는 이메일"
+          onChange={event => {
+            setIdValue(handleIdInput(event));
+          }}
+        />
+        <input
+          type="password"
+          placeholder="비밀번호"
+          onChange={event => {
+            setPassword(handleIdInput(event));
+          }}
+        />
         <button
           onClick={() => {
             goToList();
@@ -22,9 +42,11 @@ function Login() {
         >
           로그인
         </button>
-      </form>
+      </div>
       <div className={styles.lostPasswordBox}>
-        <Link to="/">비밀번호를 잊으셨나요?</Link>
+        <Link to="/" className={styles.ghLink}>
+          비밀번호를 잊으셨나요?
+        </Link>
       </div>
     </section>
   );
