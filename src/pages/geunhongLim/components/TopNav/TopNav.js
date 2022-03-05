2b55c10b1ffc2a082b1fac3039bf357852632856
geunhong Lim <seelime11@gmail.com>
2022-03-05 21:13:37 +0900

    [fix] nav 컴포넌트 오류수정

--- src/pages/geunhongLim/components/TopNav/TopNav.js
+++ src/pages/geunhongLim/components/TopNav/TopNav.js
@@ -1,14 +1,14 @@
-import styles from './Nav.scss';
+import styles from './Nav.module.scss';
 
 function Nav() {
   return (
     <header>
       <div className={styles.headerWrap}>
-        <figure className={styles.logo}>
-          <img src="images/logo.png" alt="logo" />
+        <figure className={styles.ghLogo}>
+          <img src="images/logo.png" alt="logo" className={styles.ghImg} />
         </figure>
         <nav className={styles.menuNavBar}>
-          <ul>
+          <ul className={styles.ghUl}>
             <li>COFFEE</li>
             <li>MENU</li>
             <li>STORE</li>
