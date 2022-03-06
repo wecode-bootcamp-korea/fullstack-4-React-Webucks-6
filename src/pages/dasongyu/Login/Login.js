2cfab161999fd16d3f0d1423bb462004b54dff91
Yumalup <sonaki970811@gmail.com>
2022-03-07 00:43:44 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/dasongyu/Login/Login.js
+++ src/pages/dasongyu/Login/Login.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import './Login.scss';
 
 function LoginComponent() {
+  const [idState, setIdState] = useState([]);
+  const [pwState, setPwState] = useState([]);
+
+  const handleIDInput = e => {
+    setIdState(e.target.value);
+  };
+
+  const handlePWInput = e => {
+    setPwState(e.target.value);
+  };
+
   return (
     <div>
       <div className="login-box">
@@ -9,14 +22,20 @@ function LoginComponent() {
         <div className="login-input">
           <input
             type="text"
+            name="ID"
             id="input-id"
             placeholder="전화번호,사용자 이름 또는 이메일"
+            value={idState}
+            onChange={handleIDInput}
           />
           <input
             type="password"
+            name="Password"
             id="input-pw"
             placeholder="비밀번호"
             minlength="8"
+            value={pwState}
+            onChange={handlePWInput}
           />
         </div>
         <div className="login-button">
