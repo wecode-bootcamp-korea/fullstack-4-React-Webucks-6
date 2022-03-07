7c4dc4b93ac6989f08a2e7e95fc01aa240504826
xxziiko <xxziiko@gmail.com>
2022-03-07 20:11:17 +0900

    Add : Mission 2 - 로그인 버튼 활성화 기능 구현

--- src/pages/jihokwon/Login/Login.js
+++ src/pages/jihokwon/Login/Login.js
@@ -1,41 +1,39 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { act } from 'react-dom/test-utils';
 import { Link, useNavigate } from 'react-router-dom';
 import './Login.scss';
 
 function LoginComponent() {
+  const navigate = useNavigate();
+  const goToList = () => {
+    navigate('/list-jihokwon');
+  };
+
   const [idValue, setIdValue] = useState('');
   const [pwValue, setPwValue] = useState('');
   const [loginBtnColor, setLoginBtnColor] = useState('#add3ea');
   const [btnActive, setBtnActive] = useState(true);
-  const navigate = useNavigate();
+
+  useEffect(() => {
+    idValue.includes('@') && pwValue.length > 4
+      ? setBtnActive(false)
+      : setBtnActive(true);
+  }, [idValue, pwValue]);
+
+  useEffect(() => {
+    idValue.includes('@') && pwValue.length > 4
+      ? setLoginBtnColor('#3e98f2')
+      : setLoginBtnColor('#add3ea');
+  }, [idValue, pwValue]);
 
   function handleIdInput(event) {
-    let inputIdValue = event.target.value;
-    setIdValue(inputIdValue);
-    console.log(inputIdValue.includes('@'));
-    return inputIdValue.includes('@');
+    setIdValue(event.target.value);
   }
 
   function handlePwInput(event) {
-    let inputPwValue = event.target.value;
-    setPwValue(inputPwValue);
-    console.log(inputPwValue.length >= 5);
-    return inputPwValue.length >= 5;
+    setPwValue(event.target.value);
   }
 
-  function changeBackgroundColor() {
-    console.log(handleIdInput);
-    console.log(handlePwInput);
-
-    handleIdInput() === true && handlePwInput() === true
-      ? setLoginBtnColor('#3e98f2')
-      : setLoginBtnColor('#add3ea');
-  }
-
-  const goToList = () => {
-    navigate('/list-jihokwon');
-  };
   return (
     <div>
       <div className="loginBox">
@@ -49,28 +47,14 @@ function LoginComponent() {
               type="text"
               placeholder="전화번호, 사용자 이름 또는 이메일"
               value={idValue}
-              onChange={event => {
-                handleIdInput(event);
-                changeBackgroundColor();
-                console.log(event.target.value);
-              }}
-              // onKeyUp={event => {
-              //   handleIdInput(event);
-              // }}
+              onChange={handleIdInput}
             />
             <input
               className="loginArea_input"
               type="password"
               placeholder="비밀번호"
               value={pwValue}
-              onChange={event => {
-                handlePwInput(event);
-                changeBackgroundColor();
-                console.log(event.target.value);
-              }}
-              // onKeyUp={event => {
-              //   handlePwInput(event);
-              // }}
+              onChange={handlePwInput}
             />
             <button
               className="loginArea_btn"
