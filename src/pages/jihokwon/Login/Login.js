8529fc820ac93e24cf08ddd482123a6d610927ca
xxziiko <xxziiko@gmail.com>
2022-03-07 16:58:47 +0900

    Modify : Login.js 구현중 임시저장

--- src/pages/jihokwon/Login/Login.js
+++ src/pages/jihokwon/Login/Login.js
@@ -1,19 +1,37 @@
 import React, { useState } from 'react';
+import { act } from 'react-dom/test-utils';
 import { Link, useNavigate } from 'react-router-dom';
 import './Login.scss';
 
 function LoginComponent() {
   const [idValue, setIdValue] = useState('');
   const [pwValue, setPwValue] = useState('');
+  const [loginBtnColor, setLoginBtnColor] = useState('#add3ea');
+  const [btnActive, setBtnActive] = useState(true);
+  const navigate = useNavigate();
 
   function handleIdInput(event) {
-    setIdValue(event.target.value);
-  }
-  function handlePwInput(event) {
-    setPwValue(event.target.value);
+    let inputIdValue = event.target.value;
+    setIdValue(inputIdValue);
+    console.log(inputIdValue.includes('@'));
+    return inputIdValue.includes('@');
   }
 
-  const navigate = useNavigate();
+  function handlePwInput(event) {
+    let inputPwValue = event.target.value;
+    setPwValue(inputPwValue);
+    console.log(inputPwValue.length >= 5);
+    return inputPwValue.length >= 5;
+  }
+
+  function changeBackgroundColor() {
+    console.log(handleIdInput);
+    console.log(handlePwInput);
+
+    handleIdInput() === true && handlePwInput() === true
+      ? setLoginBtnColor('#3e98f2')
+      : setLoginBtnColor('#add3ea');
+  }
 
   const goToList = () => {
     navigate('/list-jihokwon');
@@ -31,16 +49,35 @@ function LoginComponent() {
               type="text"
               placeholder="전화번호, 사용자 이름 또는 이메일"
               value={idValue}
-              onChange={handleIdInput}
+              onChange={event => {
+                handleIdInput(event);
+                changeBackgroundColor();
+                console.log(event.target.value);
+              }}
+              // onKeyUp={event => {
+              //   handleIdInput(event);
+              // }}
             />
             <input
               className="loginArea_input"
               type="password"
               placeholder="비밀번호"
               value={pwValue}
-              onChange={handlePwInput}
+              onChange={event => {
+                handlePwInput(event);
+                changeBackgroundColor();
+                console.log(event.target.value);
+              }}
+              // onKeyUp={event => {
+              //   handlePwInput(event);
+              // }}
             />
-            <button className="loginArea_btn" onClick={goToList}>
+            <button
+              className="loginArea_btn"
+              disabled={btnActive}
+              onClick={goToList}
+              style={{ backgroundColor: loginBtnColor }}
+            >
               로그인
             </button>
           </div>
