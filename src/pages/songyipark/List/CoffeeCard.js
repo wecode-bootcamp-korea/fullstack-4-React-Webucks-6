4586d809814ace3815099e3e269bc9f35554742b
songyi-p <songyi7091@gmail.com>
2022-03-10 16:15:12 +0900

    [Add] Mission6 : 커피 리스트 페이지 커피별 좋아요 기능 구현하기

--- src/pages/songyipark/List/CoffeeCard.js
+++ src/pages/songyipark/List/CoffeeCard.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import { React, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function CoffeeCard({ listData }) {
+  const [faIcon, setFaIcon] = useState('fa-regular');
+
+  const changeIcon = () => {
+    faIcon === 'fa-regular' ? setFaIcon('fa-solid') : setFaIcon('fa-regular');
+  };
+
   return (
     <div className="list-wrapper">
       <Link to="/detail-songyipark">
         <img className="coffee-img" src={listData.image} alt={listData.name} />
       </Link>
-      <div> {listData.name} </div>
+      <span> {listData.name} </span>
+      <i className={`${faIcon} fa-heart`} onClick={changeIcon} />
     </div>
   );
 }
